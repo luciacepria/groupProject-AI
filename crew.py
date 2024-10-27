2d264f28379b596c50529eb7b33974d99117d8a1
lucia <luciacepriasabater@gmail.com>
2024-10-27 21:29:35 +0000

    Sprint1

---
+++ crew.py
@@ -0,0 +1,45 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+
+@CrewBase
+class ACTCrew:
+    agents_config = 'agents.yaml'
+    tasks_config = 'tasks.yaml'
+
+    def _init_(self) -> None:
+        pass
+
+    @agent
+    def researcher(self) -> Agent:
+        return Agent(
+            config=self.agents_config['researcher']
+        )
+
+    @agent
+    def accountant(self) -> Agent:
+        return Agent(
+            config=self.agents_config['accountant']
+        )
+
+    @task
+    def researcher_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['researcher_task'],
+            agent=self.researcher()
+        )
+
+    @task
+    def accountant_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['accountant_task'],
+            agent=self.accountant()
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        return Crew(
+            agents=[self.researcher(), self.accountant()],
+            tasks=[self.researcher_task(), self.accountant_task()],
+            process=Process.sequential,
+            verbose=True
+        )
---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import time
+from dotenv import load_dotenv
+from crew import ACTCrew
+from litellm.exceptions import RateLimitError
+import openai
+
+load_dotenv()
+
+openai.api_key = os.getenv('YOUR_KEY')
+
+def run():
+    inputs = {
+        'company_name': 'Tesla', #Example for testing
+    }
+
+    crew_instance = ACTCrew()
+
+    max_retries = 3
+    for attempt in range(max_retries):
+        try:
+            crew_instance.crew().kickoff(inputs=inputs)
+            break
+        except RateLimitError as e:
+            print(f"The number of attempts has been exceeded, retrying... {attempt + 1}/{max_retries}")
+            time.sleep(2 ** attempt)
+        except Exception as e:
+            print(f"Error: {e}")
+            break
+
+if __name__ == "__main__":
+    run()
